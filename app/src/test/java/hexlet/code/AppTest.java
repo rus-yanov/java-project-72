43373626979a2eb558f8fc8eb18386f633dd6ab5
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2023-05-04 18:14:34 +0300

    fix tests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -67,14 +67,13 @@ public class AppTest {
     void testUrl() {
 
         HttpResponse<String> response = Unirest
-                .get(baseUrl + "/articles/1")
+                .get(baseUrl + "/urls/2")
                 .asString();
-
         String content = response.getBody();
 
         assertThat(response.getStatus()).isEqualTo(200);
-        assertThat(content).contains("https://youtube.com");
-        assertThat(content).doesNotContain("https://ru.hexlet.io");
+        assertThat(content).contains("https://ru.hexlet.io");
+        assertThat(content).doesNotContain("https://youtube.com");
     }
 
     @Test
